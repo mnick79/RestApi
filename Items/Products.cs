6f95165f125e535b980ef3e7c47ffa1b88931020
mnick79 <mnick79@mail.ru>
2022-02-27 14:42:14 +0300

    Adding a Product Class and Controller

---
+++ Items/Products.cs
@@ -0,0 +1,39 @@
+﻿namespace RestApi.Items
+{
+    public class Products
+    {
+        private int number;
+        private string name;
+        private decimal price;
+
+        public Products(int number, string name, decimal price)
+        {
+            this.number = number;
+            this.name = name;
+            this.price = price;
+        }
+
+
+        public int Number 
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+    }
+}
