c4b1daacbe42601a5bb9c43f81d816e9f95095f1
mnick79 <mnick79@mail.ru>
2022-02-28 23:03:08 +0300

    Подмена учебной БД mnick на demoshop (после завершения написания скрипта start.bat)

--- ConnBD/ConnectDB.cs
+++ ConnBD/ConnectDB.cs
@@ -6,7 +6,7 @@ namespace RestApi.ConnBD
     // Подключение к постгрес БД mnick, БД не закрывается после выполнения метода.
     public class ConnectDB
     {
-        private static readonly string _connectionString = @"Server=localhost;Port=5432;User Id=postgres;Password=123454321;Database=mnick;";
+        private static readonly string _connectionString = @"Server=localhost;Port=5432;User Id=postgres;Password=123454321;Database=demoshop;";
         public static NpgsqlConnection Connect()
         {
             NpgsqlConnection conn = new NpgsqlConnection(_connectionString);
