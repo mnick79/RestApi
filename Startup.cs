db71f033e93d91509c906aa120495a155f772d56
mnick79 <mnick79@mail.ru>
2022-03-30 10:54:49 +0300

    Добавление в Get пакет FluentValidation.AspNetCore и добавление в сервис

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,7 @@ namespace RestApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestApi", Version = "v1" });
             });
+            services.AddFluentValidation(c => c.RegisterValidatorsFromAssemblyContaining<Startup>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
